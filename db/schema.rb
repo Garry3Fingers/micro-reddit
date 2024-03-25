f180d5e3283c87db59068ad3f1d88390895761c4
Garry3Fingers <www.vlad78@yandex.ru>
2024-03-25 20:34:17 +0300

    Create model Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_22_182751) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_25_173112) do
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "nick_name"
     t.string "email"
@@ -22,4 +31,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_03_22_182751) do
     t.integer "age"
   end
 
+  add_foreign_key "posts", "users"
 end
